fdb1bc19611b30795e15c2ad81a93321a7012289
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-22 18:49:54 +0200

    update

--- spring-boot-security-jwt/src/main/java/repository/RoleRepository.java
+++ spring-boot-security-jwt/src/main/java/repository/RoleRepository.java
@@ -1,5 +1,15 @@
 package repository;
 
-public interface RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import models.ERole;
+import models.Role;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Optional<Role> findByName(ERole name);
 }
--- spring-boot-security-jwt/src/main/java/repository/UserRepository.java
+++ spring-boot-security-jwt/src/main/java/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package repository;
 
-public interface UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
 }
